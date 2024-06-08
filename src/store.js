54f62bd2b9d0c885ae64cf733a8ab6926fcdac08
gianluigi7 <gianluigiferraioli7@gmail.com>
2024-06-09 00:53:42 +0200

    milestone 0

---
+++ src/store.js
@@ -0,0 +1,12 @@
+import { reactive } from "vue";
+
+export const store = reactrive({
+    ApiInfo: {
+        Key: '7b087c7a40cfaccf9a035cddd5f8305b',
+        Url: 'https://api.themoviedb.org/3/search/',
+        Endpoints: {
+            movie,
+            tv, 
+    }
+    }
+});
