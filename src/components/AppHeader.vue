b0092fa0b9131d466cb14d71acf30fa82205a8f2
gianluigi7 <gianluigiferraioli7@gmail.com>
2024-06-09 02:06:25 +0200

    milestone 1

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,11 +1,51 @@
 <script>
+import axios from 'axios';
+import { store } from '../store';
  export default {
     name: 'AppHeader',
+    data() {
+      return {
+         store,
+      };
+    },
+    methods: {
+      AvviaRicerca() {
+         const url = this.store.apiInfo.Url + this.store.apiInfo.endpoints.movie;
+         console.log(url);
+         axios
+         .get(url, {
+            params: {
+               api_key: this.store.apiInfo.key,
+               query: this.store.searchKey,
+            }
+         })
+         .then(response => {
+            console.log(response.data.results)
+            this.store.movies = response.data.results;
+            console.log( this.store.movies)
+       });
+         
+      }
+    }
  }
 </script>
 
 <template>
- <h1>template vue/vite</h1>
+ <h1>BoolFlix</h1>
+ <label for="search-movie">cerca un film</label>
+ <input type="text" id="search-movie" v-model="store.searchKey">
+ <button @click="AvviaRicerca">Cerca</button>
+  
+  <ul>
+    <li v-for="movie in store.movies">
+      <div>
+         <p>Titolo: {{ movie.title }}</p>
+         <p>Titolo originale: {{ movie.original_title }}</p>
+         <p>Lingua: {{ movie.original_language }}</p>
+         <p>Voto: {{ movie.vote_average }}</p>
+      </div>
+    </li>
+  </ul>
 </template>
 
 <style scoped lang="scss">
--- src/store.js
+++ src/store.js
@@ -1,12 +1,15 @@
 import { reactive } from "vue";
 
-export const store = reactrive({
-    ApiInfo: {
-        Key: '7b087c7a40cfaccf9a035cddd5f8305b',
+export const store = reactive({
+    searchKey: '',
+    movies: [],
+    slanguageKey: 'it-IT',
+    apiInfo: {
+        key: '7b087c7a40cfaccf9a035cddd5f8305b',
         Url: 'https://api.themoviedb.org/3/search/',
-        Endpoints: {
-            movie,
-            tv, 
+        endpoints: {
+            movie: 'movie',
+            tv: 'tv',
     }
     }
 });
